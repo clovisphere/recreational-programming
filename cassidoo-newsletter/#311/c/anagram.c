58b451dfebbb32bef2aa6f9d4f0378be37f2ca35
clovisphere <clovis.mugaruka@gmail.com>
2023-08-01 15:24:27 +0300

    Improved c solution

--- cassidoo-newsletter/#311/c/anagram.c
+++ cassidoo-newsletter/#311/c/anagram.c
@@ -1,25 +1,24 @@
-// Given two strings s and t, return true if t is an anagram of s,
-// and false otherwise.
+// given two strings `s` and `t` returns `true` if they're anagrams,
+// false otherwise
 #include <stdbool.h>
 
-#define NUMBER_OF_CHAR 26
+#define HISTOGRAM_SIZE 256
 
-bool isAnagram(char *s, char *t) {
-  int freqS[NUMBER_OF_CHAR] = {0}, freqT[NUMBER_OF_CHAR] = {0}, i;
-  for (i = 0; s[i] && t[i]; i++) {
-    freqS[s[i] - 'a']++;
-    freqT[t[i] - 'a']++;
+bool is_anagram(char *s, char *t) {
+  int histogram[HISTOGRAM_SIZE] = {0}, i = 0;
+
+  for (; s[i] && t[i]; i++) {
+    histogram[s[i] - 'a']++;
+    histogram[t[i] - 'a']--;
   }
   // if 's' and 't' are of different length, they cannot be anagram
-  if (s[i] || s[i]) {
+  // 💡 this little trick checks if `s` and `t` have the same size or length
+  if (s[i] || t[i])
     return false;
-  }
-  // for an anagram, both 's' and 't' should have the same value in their
-  // respective frequency, i.e 'freqS' & 'freqT'
-  for (i = 0; i < NUMBER_OF_CHAR; i++) {
-    if (freqS[i] != freqT[i]) {
+  // valid anagram should have the `histogram` filled with `0s`:-)
+  for (i = 0; i < HISTOGRAM_SIZE; i++) {
+    if (histogram[i])
       return false;
-    }
   }
   return true;
 }
